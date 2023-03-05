3de9938e25e6b6c33764fb1180c79316f16fc223
Edgard Damiani <edamiani@gmail.com>
2023-03-05 10:56:03 +0100

    Changed bracers convention.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <SDL2/SDL.h>
 
-class GuiComponent {
+class GuiComponent 
+{
 public:
 	virtual ~GuiComponent() = default;
 
@@ -11,36 +12,38 @@ public:
 	virtual void update() {}
 	virtual void render() {}
 
-	virtual bool containsPoint(int x, int y) const {
-		return false;
-	}
+	virtual bool containsPoint(int x, int y) const { return false; }
 
-	virtual bool isDragging() const {
-		return false;
-	}
+	virtual bool isDragging() const { return false; }
 };
 
-class DraggableRectangle : public GuiComponent {
+class DraggableRectangle : public GuiComponent 
+{
 public:
 	DraggableRectangle(SDL_Renderer* renderer, const SDL_Rect& rect, const SDL_Color& color)
 		: mRenderer(renderer), mRect(rect), mColor(color) {}
 
-	void handleEvent(const SDL_Event& event) override {
-		if (event.type == SDL_MOUSEBUTTONDOWN) {
+	void handleEvent(const SDL_Event& event) override 
+	{
+		if (event.type == SDL_MOUSEBUTTONDOWN) 
+		{
 			int x, y;
 			SDL_GetMouseState(&x, &y);
 
 			SDL_Point mousePoint = { x, y };
-			if (SDL_PointInRect(&mousePoint, &mRect)) {
+			if (SDL_PointInRect(&mousePoint, &mRect)) 
+			{
 				mDragging = true;
 				mDragOffsetX = x - mRect.x;
 				mDragOffsetY = y - mRect.y;
 			}
 		}
-		else if (event.type == SDL_MOUSEBUTTONUP) {
+		else if (event.type == SDL_MOUSEBUTTONUP) 
+		{
 			mDragging = false;
 		}
-		else if (event.type == SDL_MOUSEMOTION && mDragging) {
+		else if (event.type == SDL_MOUSEMOTION && mDragging) 
+		{
 			int x, y;
 			SDL_GetMouseState(&x, &y);
 
@@ -49,16 +52,19 @@ public:
 		}
 	}
 
-	void render() override {
+	void render() override 
+	{
 		SDL_SetRenderDrawColor(mRenderer, mColor.r, mColor.g, mColor.b, mColor.a);
 		SDL_RenderFillRect(mRenderer, &mRect);
 	}
 
-	bool isDragging() const override {
+	bool isDragging() const override 
+	{
 		return mDragging;
 	}
 
-	DraggableRectangle* clone() const override {
+	DraggableRectangle* clone() const override 
+	{
 		return new DraggableRectangle(*this);
 	}
 
@@ -72,14 +78,18 @@ private:
 };
 
 
-class GuiController {
+class GuiController 
+{
 public:
 	GuiController(SDL_Renderer* renderer) : mRenderer(renderer) {}
 
-	void handleEvent(const SDL_Event& event) {
-		for (auto& component : mComponents) {
+	void handleEvent(const SDL_Event& event) 
+	{
+		for (auto& component : mComponents) 
+		{
 			component->handleEvent(event);
-			if (component->isDragging()) {
+			if (component->isDragging()) 
+			{
 				std::unique_ptr<GuiComponent> cloned = std::unique_ptr<GuiComponent>(component->clone());
 				mComponents.erase(std::remove(mComponents.begin(), mComponents.end(), component), mComponents.end());
 				mComponents.push_back(std::move(cloned));
@@ -88,18 +98,21 @@ public:
 		}
 	}
 
-	void render() {
+	void render() 
+	{
 		SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255); // Set background color to white with full alpha
 		SDL_RenderClear(mRenderer); // Clear the screen with the background color
 
-		for (auto& component : mComponents) {
+		for (auto& component : mComponents) 
+		{
 			component->render();
 		}
 
 		SDL_RenderPresent(mRenderer); // Show what has been drawn so far
 	}
 
-	void addComponent(std::unique_ptr<GuiComponent> component) {
+	void addComponent(std::unique_ptr<GuiComponent> component) 
+	{
 		mComponents.push_back(std::move(component));
 	}
 
@@ -108,7 +121,8 @@ private:
 	std::vector<std::unique_ptr<GuiComponent>> mComponents;
 };
 
-SDL_Color getRandomColor() {
+SDL_Color getRandomColor() 
+{
 	SDL_Color color = { 0, 0, 0, 255 }; // Initialize to black with full alpha
 	color.r = rand() % 256; // Random red component (0-255)
 	color.g = rand() % 256; // Random green component (0-255)
@@ -116,7 +130,8 @@ SDL_Color getRandomColor() {
 	return color;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) 
+{
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* window = SDL_CreateWindow("Draggable Rectangles", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
@@ -132,19 +147,25 @@ int main(int argc, char* argv[]) {
 	controller.addComponent(std::make_unique<DraggableRectangle>(renderer, SDL_Rect{ 400, 50, 150, 150 }, getRandomColor()));
 
 	bool running = true;
-	while (running) {
+	while (running) 
+	{
 		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+		while (SDL_PollEvent(&event)) 
+		{
+			if (event.type == SDL_QUIT) 
+			{
 				running = false;
 			}
-			else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+			else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) 
+			{
 				running = false;
 			}
-			else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r && SDL_GetModState() & KMOD_CTRL) {
+			else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r && SDL_GetModState() & KMOD_CTRL) 
+			{
 				controller.addComponent(std::make_unique<DraggableRectangle>(renderer, SDL_Rect{ rand() % 500, rand() % 300, 100, 100 }, getRandomColor()));
 			}
-			else {
+			else 
+			{
 				controller.handleEvent(event); // Handle events with the GuiController
 			}
 		}
